fec8ac6a0261190e3bc79d5f9c2649b6e7416055
shinoshyun <8367shyun@gmail.com>
2022-09-30 22:41:58 +0800

    Update week-2.py

--- week-2.py
+++ week-2.py
@@ -1,17 +1,2 @@
-# Q1
-
-# def calculate(min, max, step):
-
-#     answer = 0
-
-#     for i in min:
-#         i <= max: i += step
-#         answer += i
-
-#     print(answer)
-
-
-# # 請用你的程式補完這個函式的區塊
-# calculate(1, 3, 1)  # 你的程式要能夠計算 1+2+3，最後印出 6
-# calculate(4, 8, 2)  # 你的程式要能夠計算 4+6+8，最後印出 18
-# calculate(-1, 2, 2)  # 你的程式要能夠計算 -1+1，最後印出 0
+data = [[3, 4, 5], [6, 7, 8]]
+print(data[0][1])
