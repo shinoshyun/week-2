16a5ec919deace4fb44c2c0d97147f3d9285a816
shinoshyun <8367shyun@gmail.com>
2022-10-01 01:12:38 +0800

    Update week-2.py

--- week-2.py
+++ week-2.py
@@ -1,2 +1,50 @@
-data = [[3, 4, 5], [6, 7, 8]]
-print(data[0][1])
+# Q1
+def calculate(min, max, step):
+
+    answer = 0
+    for i in range(min, max+1, step):
+        answer += i
+
+    print(answer)
+
+
+calculate(1, 3, 1)  # 你的程式要能夠計算 1+2+3，最後印出 6
+calculate(4, 8, 2)  # 你的程式要能夠計算 4+6+8，最後印出 18
+calculate(-1, 2, 2)  # 你的程式要能夠計算 -1+1，最後印出 0
+
+
+# Q2
+def avg(data):
+
+    averageSalary = 0
+    count = 0
+    total = 0
+
+    for i in range(0, data.employees.len):
+        isManager = data.employees[i].manager
+
+
+avg({
+    "employees": [
+        {
+            "name": "John",
+            "salary": 30000,
+            "manager": False
+        },
+        {
+            "name": "Bob",
+            "salary": 60000,
+            "manager": True
+        },
+        {
+            "name": "Jenny",
+            "salary": 50000,
+            "manager": False
+        },
+        {
+            "name": "Tony",
+            "salary": 40000,
+            "manager": False
+        }
+    ]
+})  # 呼叫 avg 函式
